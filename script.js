612631956518a65836209cc8dc909785fdfb4632
Fede180600 <fede180600@libero.it>
2022-08-05 23:02:40 +0200

    app to deploy

---
+++ script.js
@@ -0,0 +1,99 @@
+// const { default: axios } = require("axios");
+
+// const { default: axios } = require("axios");
+
+const wrapper = document.querySelector(".wrapper"),
+inputPart = wrapper.querySelector(".input-part"),
+infoTxt = inputPart.querySelector(".info-txt"),
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button"),
+wIcon = document.querySelector(".weather-part img"),
+arrowBack = wrapper.querySelector("header i");
+
+inputField.addEventListener("keyup", e =>{
+    //Se l'utente preme il btn enter e l'input non è vuoto
+    if(e.key == "Enter" && inputField.value != "") {
+        requestApi(inputField.value);
+    }
+});
+
+locationBtn.addEventListener("click", () => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    } else {
+        alert("Your browser not support geolocation api")
+    }
+});
+
+function onSuccess(position) {
+    const {latitude, longitude} = position.coords;
+    fetchData();
+    axios
+    .get("https://api.openweathermap.org/data/2.5/weather?units=metric", {
+        params: {
+            lat: latitude,
+            lon: longitude,
+            appid: "79daedad5afe92bac06897ff9610a313"
+
+        }
+    })
+    .then(function(resp) {
+        weatherDetails(resp.data);
+    })
+}
+
+function onError(error) {
+    infoTxt.innerText = error.message
+    infoTxt.classList.add("error");
+}
+
+function requestApi(city) {
+    fetchData();
+    axios
+    .get("https://api.openweathermap.org/data/2.5/weather?units=metric", {params: {
+        q: city,
+        appid: "79daedad5afe92bac06897ff9610a313"
+    }})
+    .then(function(resp) {
+        weatherDetails(resp.data)
+    });
+}
+
+function fetchData() {
+    infoTxt.innerText = "Getting weather details..";
+    infoTxt.classList.add("pending");
+}
+
+function weatherDetails(info) {
+    const city = info.name;
+    const country = info.sys.country;
+    const {description, id} = info.weather[0];
+    const {feels_like, humidity, temp} = info.main;
+
+    if (id == 800) {
+        wIcon.src = "icons/sun.png"
+    } else if (id >= 200 && id <= 232) {
+        wIcon.src = "icons/storm.png"
+    } else if (id >= 600 && id <= 622) {
+        wIcon.src = "icons/snow.png"
+    } else if (id >= 701 && id <= 781) {
+        wIcon.src = "icons/haze.png"
+    } else if (id >= 801 && id <= 801) {
+        wIcon.src = "icons/cloudy.png"
+    } else if ((id >= 300 && id <= 321) || (id >= 500 && id <=531)) {
+        wIcon.src = "icons/rainy.png"
+    }
+
+    wrapper.querySelector(".temp .numb").innerText = Math.floor(temp);
+    wrapper.querySelector(".weather").innerText = description;
+    wrapper.querySelector(".location span").innerText = `${city}, ${country}`;
+    wrapper.querySelector(".temp .numb-2").innerText = Math.floor(feels_like);
+    wrapper.querySelector(".humidity span").innerText = `${humidity}%`;
+
+
+    wrapper.classList.add("active");
+}
+
+arrowBack.addEventListener("click", () => {
+    wrapper.classList.remove("active");
+})
